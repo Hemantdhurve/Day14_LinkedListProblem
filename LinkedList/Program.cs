4c3f919e770eff4caf9371d164f009906ddd603f
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-19 02:40:27 +0530

    [Hemant] Added UC1 Create Simple LinkedList

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinkedList;
+using System;
 
 namespace LinkList
 {
@@ -7,6 +8,41 @@ namespace LinkList
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Problems");
+
+            bool check=true;
+
+            Start:
+            while (check)
+            {
+                Console.WriteLine("Please Select options :\n" +
+                    "1)Elements Added in the Linked List using Add Method\n");
+
+                int option=Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Adding Elements as follows :");
+                        LinkedListData list=new LinkedListData();
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        list.Display();
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break ;
+                        goto Start;
+
+
+                }
+                
+            }
+            
         } 
     }
 }
